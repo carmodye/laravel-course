709588cdb95d024218a72a9e7335b51a42f2c20a
carmodye <e.carmody@comcast.net>
2025-05-21 14:31:14 -0400

    components and slots

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,15 @@
+@props(['color', 'bgColor'])
+
+<div {{ $attributes
+->merge(['lang' => "ka"])
+->class("card card-text-$color card-bg-$bgColor") }}>
+    <div class="card-header">{{ $title }}
+
+    </div>
+        @if ($slot->isEmpty())
+    <p>Please provide some content</p>
+@else
+    {{ $slot }}
+@endif
+    <div class="card-footer">{{ $footer }}</div>
+</div>
